dcd7127b1b0b3e9cd42569c768ccc22e6b009f88
FrancaTM <francatm@outlook.com>
2019-06-21 12:39:20 -0300

    adiciona progress indicator

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -2,11 +2,19 @@ import 'package:cars_sample/domain/login_service.dart';
 import 'package:flutter/material.dart';
 import 'package:cars_sample/utils/alerts.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   final _loginController = TextEditingController(text: '');
+
   final _senhaController = TextEditingController(text: '');
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  var _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -72,10 +80,14 @@ class LoginPage extends StatelessWidget {
             height: 50.0,
             child: RaisedButton(
               color: Colors.blue,
-              child: Text(
-                'Login',
-                style: TextStyle(fontSize: 25.0, color: Colors.white),
-              ),
+              child: _isLoading
+                  ? CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation(Colors.white),
+                    )
+                  : Text(
+                      'Login',
+                      style: TextStyle(fontSize: 25.0, color: Colors.white),
+                    ),
               onPressed: () {
                 _onPressedLogin(context);
               },
@@ -94,6 +106,10 @@ class LoginPage extends StatelessWidget {
       return;
     }
 
+    setState(() {
+      _isLoading = true;
+    });
+
     final response = await LoginService.login(login, senha);
 
     if (response.isOk()) {
@@ -101,6 +117,10 @@ class LoginPage extends StatelessWidget {
     } else {
       alert(context, 'Erro', response.message);
     }
+
+    setState(() {
+      _isLoading = false;
+    });
 
 /*
     if (login.isEmpty || senha.isEmpty) {
