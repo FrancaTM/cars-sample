9ec82a0a8004b69e6702ab0c1db23902a5a98e33
FrancaTM <francatm@outlook.com>
2019-07-02 16:14:24 -0300

    cria tabbar - parte 2 - cria carros list view widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:cars_sample/domain/carro.dart';
-import 'package:cars_sample/domain/services/carro_service.dart';
+import 'package:cars_sample/widgets/carros_list_view.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -18,80 +18,11 @@ class HomePage extends StatelessWidget {
           ]),
         ),
         body: TabBarView(children: [
-          _buildBody(TipoCarro.classicos),
-          _buildBody(TipoCarro.esportivos),
-          _buildBody(TipoCarro.luxo),
+          CarrosListView(tipo: TipoCarro.classicos),
+          CarrosListView(tipo: TipoCarro.esportivos),
+          CarrosListView(tipo: TipoCarro.luxo),
         ]),
       ),
-    );
-  }
-
-  _buildBody(String tipo) {
-    Future future = CarroService.getCarros(tipo);
-
-    return Container(
-      padding: EdgeInsets.all(16.0),
-      child: FutureBuilder<List<Carro>>(
-        future: future,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return _buildListView(snapshot.data);
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text(
-                'Dados inexistentes',
-                style: TextStyle(fontSize: 26.0, color: Colors.grey),
-              ),
-            );
-          } else {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
-      ),
-    );
-  }
-
-  _buildListView(List<Carro> carros) {
-    return ListView.builder(
-      itemCount: carros.length,
-      itemBuilder: (context, index) {
-        final c = carros[index];
-
-        return Container(
-          child: Card(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 12.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Center(child: Image.network(c.urlFoto)),
-                  Text(
-                    c.nome,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(color: Colors.black, fontSize: 24.0),
-                  ),
-                  Text(
-                    c.desc,
-                    maxLines: 1,
-                    style: TextStyle(color: Colors.grey, fontSize: 16.0),
-                  ),
-                  ButtonTheme.bar(
-                    child: ButtonBar(
-                      children: <Widget>[
-                        FlatButton(onPressed: () {}, child: Text('DETALHES')),
-                        FlatButton(onPressed: () {}, child: Text('SHARE')),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        );
-      },
     );
   }
 }
---
+++ lib/widgets/carros_list_view.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+import 'package:cars_sample/domain/carro.dart';
+import 'package:cars_sample/domain/services/carro_service.dart';
+
+class CarrosListView extends StatelessWidget {
+  final String tipo;
+
+  const CarrosListView({this.tipo});
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildBody(tipo);
+  }
+
+  _buildBody(String tipo) {
+    Future future = CarroService.getCarros(tipo);
+
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: FutureBuilder<List<Carro>>(
+        future: future,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return _buildListView(snapshot.data);
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                'Dados inexistentes',
+                style: TextStyle(fontSize: 26.0, color: Colors.grey),
+              ),
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  _buildListView(List<Carro> carros) {
+    return ListView.builder(
+      itemCount: carros.length,
+      itemBuilder: (context, index) {
+        final c = carros[index];
+
+        return Container(
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Center(child: Image.network(c.urlFoto)),
+                  Text(
+                    c.nome,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(color: Colors.black, fontSize: 24.0),
+                  ),
+                  Text(
+                    c.desc,
+                    maxLines: 1,
+                    style: TextStyle(color: Colors.grey, fontSize: 16.0),
+                  ),
+                  ButtonTheme.bar(
+                    child: ButtonBar(
+                      children: <Widget>[
+                        FlatButton(onPressed: () {}, child: Text('DETALHES')),
+                        FlatButton(onPressed: () {}, child: Text('SHARE')),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
