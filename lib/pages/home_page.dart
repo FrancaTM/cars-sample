34ad9556994ee16ec3b60421edcfe42e630d1c89
FrancaTM <francatm@outlook.com>
2019-07-02 21:49:33 -0300

    adiciona tab controller

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,25 +3,50 @@ import 'package:flutter/material.dart';
 import 'package:cars_sample/domain/carro.dart';
 import 'package:cars_sample/widgets/carros_list_view.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin<HomePage> {
+  TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    tabController = TabController(length: 3, vsync: this);
+
+    tabController.index = 1;
+
+    tabController.addListener(() {
+      int index = tabController.index;
+      print('tab $index');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Home'),
-          bottom: TabBar(tabs: [
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        bottom: TabBar(
+          controller: tabController,
+          tabs: [
             Tab(text: 'Clássicos', icon: Icon(Icons.directions_car)),
             Tab(text: 'Esportivos', icon: Icon(Icons.directions_car)),
             Tab(text: 'Luxo', icon: Icon(Icons.directions_car)),
-          ]),
+          ],
         ),
-        body: TabBarView(children: [
+      ),
+      body: TabBarView(
+        controller: tabController,
+        children: [
           CarrosListView(tipo: TipoCarro.classicos),
           CarrosListView(tipo: TipoCarro.esportivos),
           CarrosListView(tipo: TipoCarro.luxo),
-        ]),
+        ],
       ),
     );
   }
