d7046cedb658f6dbc18b30dcfabb5bf5a77710a2
FrancaTM <francatm@outlook.com>
2019-07-04 00:14:34 -0300

    adiciona tela de detalhes (parte 3)

--- lib/pages/carro_page.dart
+++ lib/pages/carro_page.dart
@@ -1,3 +1,4 @@
+import 'package:cars_sample/domain/services/carro_service.dart';
 import 'package:flutter/material.dart';
 
 import 'package:cars_sample/domain/carro.dart';
@@ -43,47 +44,78 @@ class _CarroPageState extends State<CarroPage> {
       padding: EdgeInsets.all(16.0),
       children: <Widget>[
         Image.network(carro.urlFoto),
-        Row(
-          children: <Widget>[
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    carro.nome,
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    carro.tipo,
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      color: Colors.grey[500],
-                    ),
-                  ),
-                ],
+        _buildMiddleRow(),
+        _buildBottomText(),
+      ],
+    );
+  }
+
+  Row _buildMiddleRow() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                carro.nome,
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            InkWell(
-              onTap: () {},
-              child: Icon(
-                Icons.favorite,
-                color: Colors.red,
-                size: 36.0,
+              Text(
+                carro.tipo,
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.grey[500],
+                ),
               ),
-            ),
-            InkWell(
-              onTap: () {},
-              child: Icon(
-                Icons.share,
-                size: 36.0,
-              ),
-            ),
-          ],
+            ],
+          ),
+        ),
+        InkWell(
+          onTap: () {},
+          child: Icon(
+            Icons.favorite,
+            color: Colors.red,
+            size: 36.0,
+          ),
+        ),
+        InkWell(
+          onTap: () {},
+          child: Icon(
+            Icons.share,
+            size: 36.0,
+          ),
         ),
       ],
+    );
+  }
+
+  _buildBottomText() {
+    return Container(
+      padding: EdgeInsets.only(top: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            carro.desc,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+          ),
+          SizedBox(height: 10.0),
+          FutureBuilder<String>(
+            future: CarroService.getLoremIpsum(),
+            builder: (context, snapshot) {
+              return snapshot.hasData
+                  ? Text(snapshot.data)
+                  : Center(
+                      child: CircularProgressIndicator(),
+                    );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
